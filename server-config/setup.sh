20c9a43cff1f75f3ebe76ee82261b24997028c5a
vijay <vijaydurai>
2026-09-04 12:45:07 +0530

    ansible

--- server-config/setup.sh
+++ server-config/setup.sh
@@ -15,14 +15,25 @@ sudo mkdir -p /etc/containers/registries.d/
 sudo cp policy.json /etc/containers/policy.json
 sudo cp registries.yaml /etc/containers/registries.d/podman-demo.yaml
 
+echo "Setting up initial Environment File..."
+sudo mkdir -p /etc/podman-demo
+echo "APP_IMAGE=148737623247.dkr.ecr.us-east-2.amazonaws.com/podman-demo:latest" | sudo tee /etc/podman-demo/image.env
+
 echo "Setting up Quadlet..."
 sudo mkdir -p /etc/containers/systemd/
 sudo cp podman-demo.container /etc/containers/systemd/
 
-echo "Reloading systemd and starting the service..."
+echo "Setting up Sigstore Keys Update Timer..."
+sudo cp update-sigstore-keys.sh /usr/local/bin/
+sudo chmod +x /usr/local/bin/update-sigstore-keys.sh
+sudo cp sigstore-keys-update.service /etc/systemd/system/
+sudo cp sigstore-keys-update.timer /etc/systemd/system/
+
+echo "Reloading systemd and starting services..."
 sudo systemctl daemon-reload
 # Quadlet automatically generates the .service file from the .container file
 sudo systemctl enable --now podman-demo.service
+sudo systemctl enable --now sigstore-keys-update.timer
 
 echo "================================================================"
 echo "Setup complete! "
---
+++ server-config/update-sigstore-keys.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "Updating Sigstore trust root (Fulcio CA and Rekor Pub)..."
+wget -qO /etc/pki/containers/fulcio_v1.crt.pem.new https://tuf-repo-cdn.sigstore.dev/targets/fulcio_v1.crt.pem
+wget -qO /etc/pki/containers/rekor.pub.new https://tuf-repo-cdn.sigstore.dev/targets/rekor.pub
+
+mv /etc/pki/containers/fulcio_v1.crt.pem.new /etc/pki/containers/fulcio_v1.crt.pem
+mv /etc/pki/containers/rekor.pub.new /etc/pki/containers/rekor.pub
+
+echo "Successfully updated Sigstore keys."
