7a6a41d2769f7556e607a4b8ba8dfa63d76e500c
vijay <vijaydurai>
2026-09-04 13:43:43 +0530

    ansible

--- server-config/update-sigstore-keys.sh
+++ server-config/update-sigstore-keys.sh
@@ -2,8 +2,8 @@
 set -e
 
 echo "Updating Sigstore trust root (Fulcio CA and Rekor Pub)..."
-wget -4 -O /etc/pki/containers/fulcio_v1.crt.pem.new https://tuf-repo-cdn.sigstore.dev/targets/fulcio_v1.crt.pem
-wget -4 -O /etc/pki/containers/rekor.pub.new https://tuf-repo-cdn.sigstore.dev/targets/rekor.pub
+wget -4 -O /etc/pki/containers/fulcio_v1.crt.pem.new https://fulcio.sigstore.dev/api/v1/rootCert
+wget -4 -O /etc/pki/containers/rekor.pub.new https://rekor.sigstore.dev/api/v1/log/publicKey
 
 mv /etc/pki/containers/fulcio_v1.crt.pem.new /etc/pki/containers/fulcio_v1.crt.pem
 mv /etc/pki/containers/rekor.pub.new /etc/pki/containers/rekor.pub
